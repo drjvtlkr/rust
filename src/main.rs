983aca1b308e4f33cd9f203c3dd50f8bb986eec1
dheeraj <dheerajrvithalkar@gmail.com>
2024-10-06 19:37:18 +0530

    variable and other stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
     println!("Hello, world!");
+
+    let x = 5;
+    let y = 10;
+    let z = 1000.0001;
+    println!("x: {x}, y: {y}, z:{z} ");
+    //println is for appending newline
+    /*if we did
+    * print!(.......) 
+    * and then,
+    * println!(......) the difference would not be significant
+    * because the later print statement is the last one.
+    */
+    //or
+    println!("x: {}, y: {}, z:{}",x,y,z);
+    /*
+    * this ensures that there is always new line 
+    * after the first print statement
+    */ 
+
+    /*  let mut a= 20;
+        for i in 0..1000 {
+           a=a+100;
+       }
+       print!("A final value: {}",a)
+    */
+    //this will throw an overflow error
+    
+    //to avoid overflow
+    //we declare during initialization
+    let mut a:i128= 20;
+    //like this
+    for i in 0..1000 {
+        a=a+100;
+    }
+    println!("A final value: {}",a)
+
+
 }
